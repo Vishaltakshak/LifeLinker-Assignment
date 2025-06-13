99557b12e05d7566e996ed85b5b32601beedfb13
Vishaltakshak <vishaltakshakvishal@gmail.com>
2025-06-13 23:08:34 +0530

    first commit

---
+++ Js/script.js
@@ -0,0 +1,14 @@
+
+  // const toggleBtn = document.getElementById("menuToggle");
+  // const navOverlay = document.getElementById("navOverlay");
+
+  // navOverlay.addEventListener("click", () => {
+  //   navOverlay.classList.toggle("open");
+  // });
+
+function toggleOverlay() {
+    document.getElementById("navOverlay").classList.toggle("open");
+  }
+function CloseNavBar(){
+  document.getElementById("navOverlay").classList.remove("open");
+}
